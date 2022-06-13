452b2b3f3ac812b2c0ac6e1339979582d7867b04
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-13 21:33:05 +0530

    function for password generation added

---
+++ script.js
@@ -0,0 +1,16 @@
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatePassword = "";
+  const typeCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  for (let i = 0; i < length; i += typeCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatePassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatePassword.slice(0, length);
+  return finalPassword;
+}
